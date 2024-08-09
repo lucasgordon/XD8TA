3e3ce2a078afaeb2b5b197b312d8b34e539147f6
Lucas Gordon <lucas.gordon@shopify.com>
2024-08-09 08:04:31 -0400

    Additional edits

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
 class HomeController < ApplicationController
   def index
+    if current_user
+      redirect_to analytics_user_path(current_user)
+    else
+      redirect_to sign_up_path
+    end
   end
 end
